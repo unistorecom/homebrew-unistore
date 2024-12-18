ca728b815eae67227f4083d49afc7f4eb95eb7d3
unistore <info@unistore.com>
2024-12-18 12:03:33 +0100

    Brew formula update for cli version 0.0.53

--- unistore-cli.rb
+++ unistore-cli.rb
@@ -5,11 +5,11 @@
 class UnistoreCli < Formula
   desc ""
   homepage "https://github.com/unistorecom/cli"
-  version "0.0.52"
+  version "0.0.53"
 
   on_macos do
-    url "https://cdn.dev.unistore.tech/public/cli/0.0.52/cli_0.0.52_darwin_all.tar.gz", using: CurlDownloadStrategy
-    sha256 "273329c1bb36e11fba669e10c34b96af7feef71d91581f807362d53e78aabbe7"
+    url "https://cdn.dev.unistore.tech/public/cli/0.0.53/cli_0.0.53_darwin_all.tar.gz", using: CurlDownloadStrategy
+    sha256 "88d2734472e2ca17cb3058c9007104b191a314f8a933d341eaa5b0372e5555fd"
 
     def install
       bin.install "cli" => "unistore"
@@ -19,8 +19,8 @@ class UnistoreCli < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://cdn.dev.unistore.tech/public/cli/0.0.52/cli_0.0.52_linux_amd64.tar.gz", using: CurlDownloadStrategy
-        sha256 "5d9600bfef42b0fb189c34b7c764a3368c665344e89a5b33ec0f30c5d0a00f17"
+        url "https://cdn.dev.unistore.tech/public/cli/0.0.53/cli_0.0.53_linux_amd64.tar.gz", using: CurlDownloadStrategy
+        sha256 "e0f091742262ea0199bf4952e79ae9aeac6890dba98d0611972ca4c15784e3d9"
 
         def install
           bin.install "cli" => "unistore"
@@ -29,8 +29,8 @@ class UnistoreCli < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://cdn.dev.unistore.tech/public/cli/0.0.52/cli_0.0.52_linux_arm64.tar.gz", using: CurlDownloadStrategy
-        sha256 "f806a6c2968fa39e9a730bc61bdc80dac96ec16a0ae5d6b75751ffd0a9bbe974"
+        url "https://cdn.dev.unistore.tech/public/cli/0.0.53/cli_0.0.53_linux_arm64.tar.gz", using: CurlDownloadStrategy
+        sha256 "e5ab15ff47d07c8d0f57534d04d839e3d169264f2ad12e0822af162f38769eac"
 
         def install
           bin.install "cli" => "unistore"
